346352e1dccd5b37b4f5d9d58ea9a5424c84ce4d
sampayan <97809644+sampayan@users.noreply.github.com>
2024-10-24 21:34:13 -0500

    prepared spells, known spells, and cantrips render with correct values per class

--- app/(tabs)/spellbook.tsx
+++ app/(tabs)/spellbook.tsx
@@ -1,78 +1,278 @@
 // Start of Selection
 
 import React, { useContext, useEffect, useState } from 'react';
-import { View, Text, TouchableOpacity, FlatList } from 'react-native';
+import { View, Text, TouchableOpacity, FlatList, Dimensions, ImageBackground } from 'react-native';
 import styles from '@/app/styles/spellbookStyles';
 import classData from '@/app/data/classData.json';
 import StatsDataContext from '../context/StatsDataContext';
 
+// TODO: Add learned spells from other wizards and scrolls
+const learnedSpellsFromOtherWizards = 0;
+const learnedSpellsFromScrolls = 0;
+
 export default function SpellbookScreen() {
     const [preparedSpellSlots, setPreparedSpellSlots] = useState<number | null>(null);
+    const [cantripSlots, setCantripSlots] = useState<number | null>(null);
+    const [allKnownSpellsSlots, setAllKnownSpellsSlots] = useState<number | null>(null);
     const { statsData, isSpellCaster } = useContext(StatsDataContext);
 
-    const handlePreparedSpellSlots = () => {
-        const characterClass = classData.find(cls => cls.value === statsData.class);
+    const characterClass = classData.find(cls => cls.value.toLowerCase() === statsData?.class?.toLowerCase());
+
+    useEffect(() => {
+        getPreparedSpellSlotsAmount();
+        getCantripSlotsAmount();
+        getAllKnownSpellsSlotsAmount();
+    }, [isSpellCaster, statsData.level, statsData.abilities, statsData.class, statsData.race]);
+
+    const getPreparedSpellSlotsAmount = () => {
         if (characterClass && isSpellCaster) {
-            const preparedSlots = classBasedValues(characterClass);
+            const preparedSlots = classFilterPreparedSpellBlocks();
             setPreparedSpellSlots(preparedSlots);
         }
     }
 
+    const classFilterPreparedSpellBlocks = () => {
+        const preparedSpellConfig: { [key: string]: { ability: string; levelModifier: (level: number) => number } } = {
+            artificer: {
+                ability: 'intelligence',
+                levelModifier: (level) => Math.floor(level / 2),
+            },
+            cleric: {
+                ability: 'wisdom',
+                levelModifier: (level) => level,
+            },
+            druid: {
+                ability: 'wisdom',
+                levelModifier: (level) => level,
+            },
+            paladin: {
+                ability: 'charisma',
+                levelModifier: (level) => level,
+            },
+            wizard: {
+                ability: 'intelligence',
+                levelModifier: (level) => level,
+            },
+        };
+        if (!characterClass) return null;
+        const config = preparedSpellConfig[characterClass.value.toLowerCase()];
+        if (!config) {
+            // Not a spellcaster or spells are castable without preparation
+            return null;
+        }
+        const preparedSlots = getAbilityModifier(config.ability) + config.levelModifier(statsData.level);
+        return preparedSlots || 0;
+    }
 
-    const classBasedValues = (characterClass: any) => {
-        switch (characterClass.value.toLowerCase()) {
-            case 'artificer':
-                return getIntelligenceModifier() + Math.floor(statsData.level / 2);
-            case 'barbarian':
-                return null;
-            case 'bard':
-                const bardClass = classData.find(cls => cls.value.toLowerCase() === 'bard');
-                const bardClassSpellsKnown = bardClass?.spellsKnown && typeof bardClass.spellsKnown === 'object' ?
-                    Object.entries(bardClass.spellsKnown).reduce((acc, [level, spells]) =>
-                        Number(level) <= statsData.level ? spells : acc
-                        , 0) : 0;
-                console.log("bardClassSpellsKnown", bardClassSpellsKnown); // Delete Later
-                return bardClassSpellsKnown ? bardClassSpellsKnown : null;
-            case 'cleric':
-            case 'druid':
-            case 'fighter':
-            case 'monk':
-            case 'paladin':
-            case 'ranger':
-            case 'rogue':
-            case 'sorcerer':
-            case 'warlock':
-            case 'wizard':
-                return 1;
-            default:
-                return 0;
+
+    const getAbilityModifier = (abilityName: string) => {
+        const abilityValue = statsData.abilities.find(ability => ability.name.toLowerCase() === abilityName.toLowerCase());
+        return abilityValue ? Math.floor((abilityValue.value - 10) / 2) : 0;
+    }
+
+    const numColumns = 6;
+    const windowWidth = Dimensions.get('window').width;
+    const itemWidth = (windowWidth - (30 + (numColumns - 1) * 10)) / numColumns;
+
+    const renderSpellBlock = () => (
+        <TouchableOpacity style={[styles.addSpellButton, { width: itemWidth }]}>
+            <ImageBackground
+                style={styles.spellButtonBackground}
+                source={{ uri: 'https://via.placeholder.com/150?text=&bg=EEEEEE' }}
+                resizeMode="cover"
+            >
+                <View style={styles.spellBlock}>
+                    <Text style={{ color: 'white' }}>Add Spell</Text>
+                </View>
+            </ImageBackground>
+        </TouchableOpacity >
+    );
+
+    const renderPreparedSpellBlocksForClass = () => {
+        if (preparedSpellSlots === 0 && preparedSpellSlots !== null) {
+            return (
+                <View style={styles.section}>
+                    <Text style={styles.label}>Prepared Spells</Text>
+                    <Text style={styles.text}>You can't use any spells yet.</Text>
+                </View>
+            );
+        } else if (preparedSpellSlots === null) {
+            return null;
+        } else {
+            return (
+                <View style={styles.section}>
+                    <Text style={styles.label}>Prepared Spells</Text>
+                    <FlatList
+                        data={Array.from({ length: preparedSpellSlots || 0 }, (_, i) => i)}
+                        renderItem={renderSpellBlock}
+                        keyExtractor={(item) => item.toString()}
+                        numColumns={numColumns}
+                        key={numColumns}
+                    />
+                </View>
+            )
         }
     }
 
-    const getIntelligenceModifier = () => {
-        const intelligenceAbility = statsData.abilities.find(ability => ability.name.toLowerCase() === 'intelligence');
-        return intelligenceAbility ? Math.floor((intelligenceAbility.value - 10) / 2) : 0;
+    const getClassCantripsKnown = () => {
+        const cantripsKnown = characterClass?.cantripsKnown && typeof characterClass.cantripsKnown === 'object' ?
+            Object.entries(characterClass.cantripsKnown).reduce((acc, [level, cantrips]) =>
+                Number(level) <= statsData.level ? cantrips : acc
+                , 0) : 0;
+        const cantripsAmount = cantripsKnown ? cantripsKnown : 0;
+        return cantripsAmount;
+    }
+    const getCantripSlotsAmount = () => {
+        if (characterClass && isSpellCaster) {
+            const cantrips = getClassCantripsKnown();
+            setCantripSlots(cantrips);
+        }
     }
 
-    useEffect(() => {
-        handlePreparedSpellSlots();
-    }, [isSpellCaster, statsData.level, statsData.abilities]);
 
-    const renderPreparedSpellBlock = () => (
-        <TouchableOpacity style={styles.addSpellButton}>
-            <Text style={{ color: 'white' }}>Add Spell</Text>
-        </TouchableOpacity>
-    );
+
+
+    const renderCantripBlock = () => (
+        <TouchableOpacity style={[styles.addCantripButton, { width: itemWidth }]}>
+            <ImageBackground
+                style={styles.cantripButtonBackground}
+                source={{ uri: 'https://via.placeholder.com/150?text=&bg=EEEEEE' }}
+                resizeMode="cover"
+            >
+                <View style={styles.cantripBlock}>
+                    <Text style={{ color: 'white' }}>Add Cantrip</Text>
+                </View>
+            </ImageBackground>
+        </TouchableOpacity >
+    )
+
+    const renderCantripBlocks = () => {
+        if (cantripSlots === null || cantripSlots === 0) {
+            return null;
+        } else {
+            return (
+                <View style={styles.section}>
+                    <Text style={styles.label}>Cantrips</Text>
+                    <FlatList
+                        data={Array.from({ length: cantripSlots || 0 }, (_, i) => i)}
+                        renderItem={renderCantripBlock}
+                        keyExtractor={(item) => item.toString()}
+                        numColumns={numColumns}
+                        key={numColumns}
+                    />
+                </View>
+            )
+        }
+    }
+
+    const getAllKnownSpellsSlotsAmount = () => {
+        if (characterClass && isSpellCaster) {
+            const allKnownSpells = classFilterAllKnownSpellsBlocks(characterClass?.value || null);
+            setAllKnownSpellsSlots(allKnownSpells);
+        }
+    }
+
+    const getClassPreparedSpellSlots = () => {
+        const classSpellsKnown = characterClass?.spellsKnown && typeof characterClass.spellsKnown === 'object' ?
+            Object.entries(characterClass.spellsKnown).reduce((acc, [level, spells]) =>
+                Number(level) <= statsData.level ? spells : acc
+                , 0) : 0;
+        const classSpellsKnownAmount = classSpellsKnown ? classSpellsKnown : 0;
+        return classSpellsKnownAmount;
+    }
+
+    const classFilterAllKnownSpellsBlocks = (characterClass: string | null) => {
+        if (!characterClass) return 0;
+
+        const lowerClass = characterClass.toLowerCase();
+
+        const preparedSpellClasses = new Set(['bard', 'ranger', 'sorcerer', 'warlock']);
+        const knownButNotPreparedClasses = new Set(['artificer', 'cleric', 'druid', 'paladin']);
+
+        if (preparedSpellClasses.has(lowerClass)) {
+            return getClassPreparedSpellSlots();
+        }
+
+        if (lowerClass === 'wizard') {
+            return statsData.level + learnedSpellsFromOtherWizards + learnedSpellsFromScrolls;
+        }
+
+        if (knownButNotPreparedClasses.has(lowerClass)) {
+            // All {Class} spells are known, but not prepared
+            return null;
+        }
+
+        // Not a spellcaster
+        return null;
+    }
+
+    const renderAllKnownSpells = () => {
+        if (allKnownSpellsSlots !== null && preparedSpellSlots === null) {
+            return null;
+        } else {
+            if (allKnownSpellsSlots === 0 && allKnownSpellsSlots !== null) {
+                return (
+                    <View style={styles.section}>
+                        <Text style={styles.label}>Known Spells</Text>
+                        <Text style={styles.text}>You can't learn any spells yet.</Text>
+                    </View>
+                )
+            } else if (allKnownSpellsSlots === null) {
+                return null;
+            } else {
+                return (
+                    <View style={styles.section}>
+                        <Text style={styles.label}>Known Spells</Text>
+                        <FlatList
+                            data={Array.from({ length: allKnownSpellsSlots || 0 }, (_, i) => i)}
+                            renderItem={renderSpellBlock}
+                            keyExtractor={(item) => item.toString()}
+                            numColumns={numColumns}
+                            key={numColumns}
+                        />
+                    </View>
+                )
+            }
+        }
+    }
+
+    const renderCastableSpells = () => {
+        if (allKnownSpellsSlots !== null && preparedSpellSlots === null) {
+            if (allKnownSpellsSlots === 0 && allKnownSpellsSlots !== null) {
+                return (
+                    <View style={styles.section}>
+                        <Text style={styles.label}>Spells</Text>
+                        <Text style={styles.text}>You can't cast any spells yet.</Text>
+                    </View>
+                )
+            } else if (allKnownSpellsSlots === null) {
+                return null;
+            } else {
+                return (
+                    <View style={styles.section}>
+                        <Text style={styles.label}>Spells</Text>
+                        <FlatList
+                            data={Array.from({ length: allKnownSpellsSlots || 0 }, (_, i) => i)}
+                            renderItem={renderSpellBlock}
+                            keyExtractor={(item) => item.toString()}
+                            numColumns={numColumns}
+                            key={numColumns}
+                        />
+                    </View>
+                )
+            }
+        } else {
+            return null;
+        }
+    }
 
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>Spellbook</Text>
-            <FlatList
-                data={Array.from({ length: preparedSpellSlots || 0 }, (_, i) => i)}
-                renderItem={renderPreparedSpellBlock}
-                keyExtractor={(item) => item.toString()}
-                contentContainerStyle={{ flexDirection: 'column' }}
-            />
+            <Text style={styles.title}>{statsData.class} Spellbook</Text>
+            {renderPreparedSpellBlocksForClass()}
+            {renderCantripBlocks()}
+            {renderAllKnownSpells()}
+            {renderCastableSpells()}
         </View>
     );
 }
